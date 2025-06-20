c979b945f874657c923e11404ac773f8c4849789
Jose Costa <josecosta14@hotmail.com>
2025-06-19 21:11:12 -0300

    Estructura Repetitiva, Sumatoria

---
+++ Sumatoria.py
@@ -0,0 +1,17 @@
+
+#Sumatoria
+cant_numeros = 5
+sumatoria = 0 
+
+for cont in range(1,cant_numeros+1):
+    print("Ingrese numero cont ", cont)
+    num = int(input())
+    sumatoria = sumatoria + num
+
+print("La sumatoria de los valores es ",sumatoria) 
+
+# while 
+contrasena = ""
+while contrasena != "python":
+     contrasena = input("Ingrese otra contraseña: ")
+print("Acceso consedido")
